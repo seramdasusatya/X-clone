341de724f8824e0061a8c75967b1fa36b819b51c
seramdasusatya <97424415+seramdasusatya@users.noreply.github.com>
2024-07-30 17:17:55 +0530

    1-auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -8,6 +8,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000 ;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 
